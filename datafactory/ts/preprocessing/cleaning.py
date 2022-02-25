b9d75ca3b5d1280c164b86aad6f7dcf5f4882a17
Yiran Huang <ihuangyiran@icloud.com>
2022-02-25 10:19:10 +0100

    save the print information in the given file

--- datafactory/ts/preprocessing/cleaning.py
+++ datafactory/ts/preprocessing/cleaning.py
@@ -12,12 +12,13 @@ import sys
 sys.path.append('../../util')
 from ...util.constants import logger, bcolors
 
-def clean_data(data: pd.DataFrame, strategy: str='model') -> pd.DataFrame:
+def clean_data(data: pd.DataFrame, strategy: str='model', file = None) -> pd.DataFrame:
     """Clean dataset of INF- and NAN-values.
     
     Keyword arguments:
     dat -- dataframe
     strategy -- cleaning strategy, should be in ['model', 'mean', 'median', 'most_frequent', 'constant']
+    file -- file to save the cleaning information
     Output:
     data -- cleaned dataframe
     """
@@ -26,20 +27,20 @@ def clean_data(data: pd.DataFrame, strategy: str='model') -> pd.DataFrame:
         
     logger.info('Start to clean the given dataframe...')
     
-    print('#'*30)
-    print('clean na and inf in the data')
-    print('#'*30)
+    print('#'*30, file = file)
+    print('clean na and inf in the data', file = file)
+    print('#'*30, file = file)
     
-    print(f'Number of INF- and NAN-values are: ({bcolors.HEADER}{bcolors.BOLD}{(data == np.inf).sum().sum()}{bcolors.ENDC}, {bcolors.HEADER}{bcolors.BOLD}{data.isna().sum().sum()}{bcolors.ENDC})')
-    print('Set type to float32 at first && deal with INF')
+    print(f'Number of INF- and NAN-values are: ({bcolors.HEADER}{bcolors.BOLD}{(data == np.inf).sum().sum()}{bcolors.ENDC}, {bcolors.HEADER}{bcolors.BOLD}{data.isna().sum().sum()}{bcolors.ENDC})', file = file)
+    print('Set type to float32 at first && deal with INF', file = file)
     
     data = data.astype(np.float32)
     data = data.replace([np.inf, -np.inf], np.nan)
     
-    print(f'Remove columns with half of NAN-values: {data.columns[(data.isna().sum()/data.shape[0])>=0.5].to_list()}')
+    print(f'Remove columns with half of NAN-values: {data.columns[(data.isna().sum()/data.shape[0])>=0.5].to_list()}', file = file)
     data = data.dropna(axis=1, thresh=data.shape[0] * .5)
     
-    print(f'Remove constant columns: {data.columns[(data == data.iloc[0]).all()].to_list()}')
+    print(f'Remove constant columns: {data.columns[(data == data.iloc[0]).all()].to_list()}', file = file)
     data = data.loc[:, (data != data.iloc[0]).any()]
 
     if data.isna().sum().sum() > 0:
@@ -67,6 +68,6 @@ def clean_data(data: pd.DataFrame, strategy: str='model') -> pd.DataFrame:
     #logger.info('Remove rows with any nan in the end')
     #data = data.dropna(axis=0, how='any')
     logger.info(f'...End with Data cleaning, number of INF- and NAN-values are now: ({(data == np.inf).sum().sum()}, {data.isna().sum().sum()})')
-    print(f'End with Data cleaning, number of INF- and NAN-values are now: ({bcolors.HEADER}{bcolors.BOLD}{(data == np.inf).sum().sum()}{bcolors.ENDC}, {bcolors.HEADER}{bcolors.BOLD}{data.isna().sum().sum()}{bcolors.ENDC})')
+    print(f'End with Data cleaning, number of INF- and NAN-values are now: ({bcolors.HEADER}{bcolors.BOLD}{(data == np.inf).sum().sum()}{bcolors.ENDC}, {bcolors.HEADER}{bcolors.BOLD}{data.isna().sum().sum()}{bcolors.ENDC})', file = file)
     #data = data.reset_index(drop=True)
     return data
