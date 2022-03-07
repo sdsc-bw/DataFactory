92621269b7e24a3ffecb5676cb04c506ec143646
Yiran Huang <ihuangyiran@icloud.com>
2022-03-07 15:47:45 +0100

    change print to markdown form

--- datafactory/ts/preprocessing/validating.py
+++ datafactory/ts/preprocessing/validating.py
@@ -46,6 +46,8 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
         the dataframe that contains the category features
     dat_y - type of DataFrame
         the serie that contains the target
+    dat_number_na - type of DataFrame
+        the number of na value in this featurs
     le_name_mapping - type of dict
         the dict that contain the mapping between the original class in the target to the new class
     flag_balance - type of bool
@@ -53,9 +55,9 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
     flag_wrong_target - type of bool
         it is True, when there are more then 10 classes contain less than 10 items
     """
-    print('#'*30, file = file)
-    print('Check data', file = file)
-    print('#'*30, file = file)
+    #print('#'*30, file = file)
+    #print('## Check data', file = file)
+    #print('#'*30, file = file)
     if logger:
         logger.info('Start to check the given data set...')
         
@@ -65,12 +67,12 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
     if y:
         # check if y exist
         while y not in dat.columns():
-            y = print('Given target not found, please input the new target name:')
+          y = print('Given target not found, please input the new target name:')
         
         # check na in y and drop na
         number_na_in_target = dat[y].isna().sum()
         if number_na_in_target > 0:
-            dat = dat[dat[y].isna().map(lambda x: not x)]
+          dat = dat[dat[y].isna().map(lambda x: not x)]
 
         dat_y = dat[y]
         cols = dat.columns.to_list()
@@ -82,7 +84,7 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
         # check na in y and drop na
         number_na_in_target = dat.iloc[:, -1].isna().sum()
         if number_na_in_target > 0:
-            dat = dat[dat.iloc[:, -1].isna().map(lambda x: not x)]
+          dat = dat[dat.iloc[:, -1].isna().map(lambda x: not x)]
 
         dat_y = dat[dat.columns[-1]]
         dat_x = dat[dat.columns[:-1]]
@@ -96,54 +98,58 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
     dat_category = dat_x.select_dtypes(include = ['object'])
     dat_numeric =  dat_x.select_dtypes(include=['float32', 'float64', 'int'])
 
-    print(f'#### basic information ####', file = file)
-    print(f'The type of the task is:{bcolors.HEADER}{bcolors.BOLD}{"Classification" if art == "C" else "Regression"}{bcolors.ENDC}, with target feature: {bcolors.HEADER}{bcolors.BOLD}{y if y else dat.columns[-1]}{bcolors.ENDC}.', file = file)
-    print(f'The given data include {bcolors.HEADER}{bcolors.BOLD}{dat.shape[1]}{bcolors.ENDC} columns and {bcolors.HEADER}{bcolors.BOLD}{dat.shape[0]}{bcolors.ENDC} rows:', file = file)
-    print(f'- {bcolors.HEADER}{bcolors.BOLD}{len(dat_numeric.columns)}{bcolors.ENDC} features are numeric: {dat_numeric.columns[:5].to_list()} {"..." if len(dat_numeric.columns)>5 else "."}', file = file)
-    print(f'- {bcolors.HEADER}{bcolors.BOLD}{len(dat_category.columns)}{bcolors.ENDC} features are category: {dat_category.columns[:5].to_list()} {"..." if len(dat_category.columns)>5 else "."}', file = file)
+    print(f'#### basic information', file = file)
+    #print(f'The type of the task is:{bcolors.HEADER}{bcolors.BOLD}{"Classification" if art == "C" else "Regression"}{bcolors.ENDC}, with target feature: {bcolors.HEADER}{bcolors.BOLD}{y if y else dat.columns[-1]}{bcolors.ENDC}.', file = file)
+    print(f'The type of the task is: ***{"Classification" if art == "C" else "Regression"}***, with target feature: ***{y if y else dat.columns[-1]}***. \n', file = file)
+    print(f'The given data include ***{dat.shape[1]}*** columns and ***{dat.shape[0]}*** rows: \n', file = file)
+    print(f'- ***{len(dat_numeric.columns)}*** features are numeric: {dat_numeric.columns[:5].to_list()} {"..." if len(dat_numeric.columns)>5 else "."} \n', file = file)
+    print(f'- ***{len(dat_category.columns)}*** features are category: {dat_category.columns[:5].to_list()} {"..." if len(dat_category.columns)>5 else "."} \n', file = file)
 
     flag_balance = False
     flag_wrong_target = False
 
     if art == 'C':
-        print(f'- target value has {bcolors.HEADER}{bcolors.BOLD}{len(dat_y.unique())}{bcolors.ENDC} different classed and is type of {bcolors.HEADER}{bcolors.BOLD}{"category" if dat_y.dtype == pd.CategoricalDtype else "numeric"}{bcolors.ENDC}', file = file)
+      print(f'- target value has ***{len(dat_y.unique())}*** different classed and is type of ***{"category" if dat_y.dtype == pd.CategoricalDtype else "numeric"}*** \n', file = file)
 
-        # collect the class information
-        tmp = dat_y.value_counts()
+      # collect the class information
+      tmp = dat_y.value_counts()
 
-        cls = {}
-        number_items_less_10 = 0
-        for i in range(tmp.shape[0]):
-            cls[tmp.index[i]] = tmp.iloc[i]
+      cls = {}
+      number_items_less_10 = 0
+      for i in range(tmp.shape[0]):
+        cls[tmp.index[i]] = tmp.iloc[i]
         if tmp.iloc[i] < 10:
-            number_items_less_10 += 1
+          number_items_less_10 += 1
       
-        # check if the given y is the right target
-        if number_items_less_10 >= 10:
-            flag_wrong_target = True
+      # check if the given y is the right target
+      if number_items_less_10 >= 10:
+        flag_wrong_target = True
 
-        # check balancy
-        if tmp.std()<100:
-            flag_balance = True
+      # check balancy
+      if tmp.std()<100:
+        flag_balance = True
       
-        if flag_balance:
-            print(f'\t The given dataset is balance: {bcolors.HEADER}{bcolors.BOLD}{cls}{bcolors.ENDC}', file = file)
-        else:
-            print(f'\t The given dataset is unblance: {bcolors.HEADER}{bcolors.BOLD}{cls}{bcolors.ENDC}', file = file)
+      if flag_balance:
+        print(f'\t The given dataset is balance: ***{cls}*** \n', file = file)
+      else:
+        print(f'\t The given dataset is unblance: ***{cls}*** \n', file = file)
 
     ## report basic flaws of the given data, which include following info:
     # - na and inf in the feature and target columns
     # - feature that contain only one value
-    print(f'#### na/inf value ####', file = file)
+    print(f'#### na/inf value', file = file)
     # na, inf in y
     dat_y = dat_y.replace(np.inf, np.nan)
-    print(f'There is in total {bcolors.HEADER}{bcolors.BOLD}{number_na_in_target}{bcolors.ENDC} NA and Inf value found in the target column, corresponding items are deleted from the given dataset.', file = file)
+    print(f'There is in total ***{number_na_in_target}*** NA and Inf value found in the target column, corresponding items are deleted from the given dataset.', file = file)
+    dat_y = dat_y[dat_y.isna().map(lambda x: not x)]
+    dat_x = dat_x[dat_y.isna().map(lambda x: not x)]
 
     # na, inf in x
     dat_x = dat_x.replace(np.inf, np.nan)
-    print(f'There is in total {bcolors.HEADER}{bcolors.BOLD}{dat_x.isna().sum().sum()}{bcolors.ENDC} NA and Inf value found in the given features data.', file = file)
+    print(f'There is in total ***{dat_x.isna().sum().sum()}*** NA and Inf value found in the given features data.', file = file)
     if dat_x.isna().sum().sum() > 0:
-        print(dat_x.isna().sum().map(lambda x: str(x) + '/' + str(dat_x.shape[0])), file = file)
+      print(dat_x.isna().sum().map(lambda x: str(x) + '/' + str(dat_x.shape[0])))
+      dat_number_na = dat_x.isna().sum().map(lambda x: str(x) + '/' + str(dat_x.shape[0]))
     
     # unique: numeric and cataegory
     tmp = dat_numeric.std() == 1
@@ -151,9 +157,9 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
     unique_column_category = []
 
     for col in dat_category.columns:
-        tmp_col = dat_category[col]
-        if len(tmp_col.unique()) == 1:
-            unique_column_category.append(col)
+      tmp_col = dat_category[col]
+      if len(tmp_col.unique()) == 1:
+        unique_column_category.append(col)
 
     ## deal with the categoric target
     # 
@@ -161,7 +167,7 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
     if art == 'C':
         if logger:
             logger.info('- start to label target feature y for classification task')
-        print(f'#### Labeling the target ####', file = file)
+        print(f'#### Labeling the target ', file = file)
         print('Relabel the target class to make it as type of numeric and start from 0', file = file)
         le = LabelEncoder()
         dat_y = le.fit_transform(dat_y)
@@ -173,6 +179,6 @@ def check_data_and_distribute(dat: pd.DataFrame, art: str = 'R', y: str = None, 
             logger.info('+ end with label encoding the target feature')
     
     if logger:
-        logger.info('...finish with data check')
+      logger.info('...finish with data check')
 
-    return dat_numeric, dat_category, dat_y, le_name_mapping, flag_balance, flag_wrong_target
+    return dat_numeric, dat_category, dat_y, dat_number_na, le_name_mapping, flag_balance, flag_wrong_target
