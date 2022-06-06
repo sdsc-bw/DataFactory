4bb25d52c7ad4b9fb6b04574efd9d8deb457b3a2
Yiran Huang <xhuangyiran@gmail.com>
2022-06-06 15:15:12 +0200

    add time series data loading method:

--- datafactory/ts/preprocessing/loading.py
+++ datafactory/ts/preprocessing/loading.py
@@ -2,7 +2,7 @@
 Copyright (c) Smart Data Solution Center Baden-Württemberg 2021,
 All rights reserved.
 '''
-
+import copy
 import pandas as pd
 import numpy as np
 import os
@@ -11,8 +11,8 @@ import sqlite3
 from sklearn.metrics import f1_score
 import sklearn.utils
 from typing import cast, Any, Dict, List, Tuple, Optional, Union
+from datetime import datetime
 import sys
-
 sys.path.append('../feature_engineering')
 from ..feature_engineering.transforming import apply_transforms
 
@@ -39,6 +39,124 @@ def split_df(df: pd.DataFrame, target_col=None):
     return X, y
     
 
+def load_dataset_from_dir(path: str, header: str = 'infer', sep: str = ',', index_col: Union[str, int] = 0, time_format: str = None, dayfirst = True):
+    """
+    Attention: timestamp should be set to index
+    Keyword arguments:
+    data_type -- type of the file, should be in ['csv', 'xml', 'txt']
+    file_path_or_buffer -- path to dataset, valid xml string or url
+    sep -- seperator of the dataset to seperate cells if csv-file
+    index_col -- name or index of the index_column, should the type of datetime
+    """
+    ## load data
+    filenames = [os.path.join(path, i) for i in os.listdir(path) if i.split('.')[-1] == 'csv']
+    
+    dfs = []
+    for ind, filename in enumerate(filenames):
+        if type(index_col) is list:
+            # load data
+            tmp = pd.read_csv(filename, header = 'infer',sep=sep)
+            tmp.index = tmp.apply(lambda x: ' '.join(x[index_col]), axis = 1) # attention, only work if index item is type of object
+            for col in index_col:
+                del tmp[col]
+        
+        else:
+            tmp = pd.read_csv(filename, sep=sep, header='infer', index_col = index_col)
+            
+        # convert column to numeric
+        tmp = _convert_df_comma_and_set_type_float(tmp)
+
+        # select only numeric column
+        numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+        cols = tmp.select_dtypes(include=numerics).columns
+        tmp = tmp[cols]
+
+        # add pref 
+        tmp.columns = [str(ind) + '_'+ i for i in tmp.columns]
+
+        dfs.append(tmp)
+            
+        
+    ## transform index type to datetime
+    index_fails = []
+    for i, df in enumerate(dfs):
+        try:
+            df.index = pd.to_datetime(df.index, format=time_format)
+        except:
+            print(f'Warning: Can not convert index of the {i} dataframe to datetime, please check the data again')
+            index_fails.append(i)
+            
+    if len(index_fails) == len(df):
+        # no file contain a meaningful datetime
+        print('None of the given files contain a meaningful datetime, generate new timestamp automatically. remember that this timestamp means nothing!!!, it will also affect the table combination later.')
+        
+        date_start = datetime.strptime("90/01/01 00:00", "%d/%m/%y %H:%M")
+        for df in dfs:
+            date_end = date_start + timedelta(hours = len(df))
+            grad = timedelta(hours = 1)
+            df.index = np.arange(date_start, date_end, grad)
+            
+    elif len(index_fails) == 0:
+        pass
+    
+    else:
+        # index of some of the files can not be convert to datetime
+        print('Some of the given files contain a illegal datetime, remove this datasets from the list')
+        tmp = []
+        [tmp.append(dfs[i])for i in index_fails]
+        dfs = tmp
+        del tmp
+    
+    ## combine multi-dataset, if necessary: list -> pd.DataFrame
+    if len(dfs) > 1:
+        # get minimal distance and datetime, get maximal datetime, recreate dataframe and load data and combine
+        li_date_start = []
+        li_date_end = []
+        li_date_gap = []
+        
+        for df in dfs:
+            li_date_start.append(df.index[0])
+            li_date_end.append(df.index[-1])
+            li_date_gap.append(df.index[1] - df.index[0]) ## ATTENTION!!!!!!!: this method is naive, replace it later.
+        
+        date_start = min(li_date_start)
+        date_end = min(li_date_end)
+        date_gap = min(li_date_gap)
+        cdf = pd.DataFrame(index = np.arange(date_start, date_end, date_gap))
+        
+        # combine data
+        df_new = []
+        for df in dfs:
+            tmp_df = []
+            for i in cdf.index:
+                tmp_df.append(df.iloc[i: i + grad].mean().values) # mean to ensure if smallest gap is not the smallest one
+                
+            df_new.append(pd.DataFrame(tmp_df, index = cdf.index))
+        
+        df_new = pd.concat(df_new, axis = 1)
+    
+    else:
+        df = dfs[0]
+    
+    return df
+
+def _convert_column_comma_and_set_type_float(col: pd.Series,) -> pd.Series:
+    return col.map(lambda x: x.replace(',', '.') if type(x) != float else x).astype(float)
+
+def _convert_df_comma_and_set_type_float(df: pd.DataFrame, verbose: bool = True) -> pd.DataFrame:
+    """
+    It's useful when in the given dataset the point of number is set to comma. This method can turn it back
+    """
+    df = copy.deepcopy(df)
+    for i in df.columns:
+        try:
+            df[i] = _convert_column_comma_and_set_type_float(df[i])
+        except:
+            if verbose:
+                print(f'column {i} is not numerical')
+            
+    return df         
+        
 def load_dataset_from_file(data_type: str, file_path_or_buffer: Union[list,str], header: str='infer', sep: str=',', index_col: Union[str, int]=0, time_format: str=None, shuffle: bool=True, transform: List=None) -> Union[pd.DataFrame, List]:    
     """Loads one or multiple datasets from the given file paths.
         
@@ -63,6 +181,7 @@ def load_dataset_from_file(data_type: str, file_path_or_buffer: Union[list,str],
             df.reset_index(drop=True, inplace=True)
             
         return df
+    
     elif type(file_path_or_buffer) == list:
         dfs = []
         for i in file_path_or_buffer:
@@ -116,9 +235,9 @@ def load_dataset_from_database(database: Union[list,str], query: Union[list,str]
         raise AttributeError(f'Unknown datatype of database: {database}')
         
 
-def _read_file(data_type: str, file_path_or_buffer: str, header: str='infer', sep: str=',', index_col: Union[int, str]=0, time_format=None):
+def _read_file(data_type: str, file_path_or_buffer: str, header: str='infer', sep: str=',', index_col: Union[int, str]=0):
     if data_type == 'csv':
-        df = pd.read_csv(file_path_or_buffer, sep=sep, header=header)
+        df = pd.read_csv(file_path_or_buffer, sep=sep, header=header, index_col = index_col)
     elif data_type == 'xml':
         df = pd.read_xml(file_path_or_buffer)
     elif data_type == 'txt':
@@ -126,11 +245,6 @@ def _read_file(data_type: str, file_path_or_buffer: str, header: str='infer', se
         
     else:
         raise AttributeError(f'Unknown datatype of file: {data_type}')
-        
-    if type(index_col) == str:
-        df[index_col] = pd.to_datetime(df[index_col], format=time_format)    
-    if index_col:
-        df = df.set_index(index_col)
         
     return df
     
